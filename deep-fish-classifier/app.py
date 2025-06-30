3ccfcb76ef93f6134cdb7b8a45e12bf7c71d80db
Teo <izpyten@yahoo.com>
2025-06-30 13:07:47 +0200

    uploading the gradio git commit -m "Add Gradio app script and requirements file for deployment"

---
+++ deep-fish-classifier/app.py
@@ -0,0 +1,14 @@
+
+from fastai.vision.all import *
+import gradio as gr
+
+learn = load_learner('deep_fish_classifier.pkl')
+
+# Prediction function
+def classify_image(img):
+    pred_class, pred_idx, probs = learn.predict(img)
+    return f"{pred_class} ({probs[pred_idx]:.2%})"
+
+# Building Gradio interface
+demo = gr.Interface(fn=classify_image, inputs="image", outputs="text")
+demo.launch()
